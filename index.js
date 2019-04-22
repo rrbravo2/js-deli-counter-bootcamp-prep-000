e7c5bb41daddec830770854adc833f3c31fcfe4c
Ryan Bravo <rrbravo2@gmail.com>
2019-04-22 21:25:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function currentLine(KatzDeliLine) {
   if (KatzDeliLine.length === 0){
     return "The line is currently empty.";
   }
-   var line = []
+   var deliLine = []
    for (let i = 0; i< KatzDeliLine.length; i++) {
-   line.push(i+1 + ". " + KatzDeliLine[i])
+   deliLine.push(i+1 + ". " + KatzDeliLine[i])
 }
-   return "The line is currently: line.join(",")"
+   return 'The line is currently: deliLine.join(',')'
 }
